aa6bfc669159535a9c786a2aa5fae351a36c5911
Isaac Lopez <rlopez@dacompsc.com>
2019-04-23 07:56:45 -0500

    Dos envios

--- traking.php
+++ traking.php
@@ -4,4 +4,4 @@ require_once('FedexConection.php');
 $obj = new FedexConection(true);
 echo '<pre>';
 
-print_r($obj->getTracking(['039813852990618']));
+print_r($obj->getTracking(['039813852990618','231300687629630']));
